a4d14b5a441ef500ebee028bbe11b631c1062921
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2021-02-18 18:55:19 +0100

    add got to top button
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I587b22137db7446f587efc8b5a5931c4b7a84262

--- src/App.js
+++ src/App.js
@@ -22,6 +22,9 @@ class App extends Component {
               </div>
             </div>
           </div>
+            <a className="scroll-to-top rounded" href="#page-top">
+                <i className="fas fa-angle-up"></i>
+            </a>
         </Router>
       )
   };
